c8739b90fdd1e62f805110be3c80e1bd0d863684
husen.zhang@gmail.com <husen.zhang@gmail.com>
2019-11-10 17:01:19 -0500

    changed endpoint name to predict

--- sentiment-clf/app.py
+++ sentiment-clf/app.py
@@ -50,7 +50,7 @@ class PredictSentiment(Resource):
 
 # Setup the Api resource routing here
 # Route the URL to the resource
-api.add_resource(PredictSentiment, '/')
+api.add_resource(PredictSentiment, '/predict')
 
 
 if __name__ == '__main__':
