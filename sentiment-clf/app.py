be4b33d4f0a598b669dd59dd824b07104992d7cb
husen.zhang@gmail.com <husen.zhang@gmail.com>
2020-04-18 15:39:33 -0400

    added health check

--- sentiment-clf/app.py
+++ sentiment-clf/app.py
@@ -7,8 +7,10 @@ from model import NLPModel
 app = Flask(__name__)
 api = Api(app)
 
-model = NLPModel()
+parser = reqparse.RequestParser()
+parser.add_argument('query')
 
+model = NLPModel()
 clf_path = 'lib/models/SentimentClassifier.pkl'
 with open(clf_path, 'rb') as f:
     model.clf = pickle.load(f)
@@ -17,40 +19,30 @@ vec_path = 'lib/models/TFIDFVectorizer.pkl'
 with open(vec_path, 'rb') as f:
     model.vectorizer = pickle.load(f)
 
-# argument parsing
-parser = reqparse.RequestParser()
-parser.add_argument('query')
-
-
 class PredictSentiment(Resource):
+
     def get(self):
-        # use parser and find the user's query
         args = parser.parse_args()
         user_query = args['query']
 
-        # vectorize the user's query and make a prediction
         uq_vectorized = model.vectorizer_transform(np.array([user_query]))
-        prediction = model.predict(uq_vectorized)
+        pred = model.predict(uq_vectorized)
         pred_proba = model.predict_proba(uq_vectorized)
 
-        # Output either 'Negative' or 'Positive' along with the score
-        if prediction == 0:
-            pred_text = 'Negative'
-        else:
-            pred_text = 'Positive'
-
-        # round the predict proba value and set to new variable
-        confidence = round(pred_proba[0], 3)
-
-        # create JSON object
-        output = {'prediction': pred_text, 'confidence': confidence}
+        output = {'args': args,
+                  'label':      pred.tolist()[0],
+                  'confidence': pred_proba[0]}
 
         return output
 
 
-# Setup the Api resource routing here
-# Route the URL to the resource
-api.add_resource(PredictSentiment, '/')
+class HealthCheck(Resource):
+    def get(self):
+        return {'status': 'OK'}
+
+# Setup URL to api resource
+api.add_resource(PredictSentiment, '/predict')
+api.add_resource(HealthCheck, '/health')
 
 
 if __name__ == '__main__':
